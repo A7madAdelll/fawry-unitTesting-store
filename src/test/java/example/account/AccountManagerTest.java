2e99b392cccc2ee3fe1710b58f7caafed0de9112
ahmed adel <official3adool@gmail.com>
2026-05-09 20:40:29 +0300

    test: test the failure of depositing negative value

---
+++ src/test/java/example/account/AccountManagerTest.java
@@ -0,0 +1,32 @@
+package example.account;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountManagerTest {
+
+    @Test
+    void givenCustomerWithSufficientBalance_whenWithdraw_thenSucceed() {
+        // Arrange
+        AccountManager am = new AccountManagerImpl();
+        Customer c = new Customer();
+        c.setBalance(1000);
+
+        // Act
+        String result = am.withdraw(c, 500);
+
+        // Assert
+        Assertions.assertEquals("success", result);
+        Assertions.assertEquals(500, c.getBalance());
+    }
+    @Test
+    void givenCustomerDepositingNegativeBalanceThenFail(){
+        AccountManager am = new AccountManagerImpl();
+        Customer c = new Customer();
+        c.setBalance(0);
+        String result=am.deposit(c,-300);
+        Assertions.assertEquals("fail", result);
+    }
+
+
+}
